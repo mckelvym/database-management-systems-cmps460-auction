614b1430af61fddc48c418f7eecd21e4106823a2
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-03-23 02:00:39 +0000

    Updated script to pass dbinfo to the echo_header() function which will show a customized header and navigation. I also added some sample code that will update the time from the database to the session vars and display it on the screen. The page just has some sample information echoed for current time, if the current user is logged in, what the current username is, and is the current user an admin

--- source/index.php
+++ source/index.php
@@ -4,36 +4,24 @@ include_once ("common.php");
 $dbinfo = new dbinfo_t ();
 $dbinfo->init ();
 
+echo_header ($dbinfo);
 
-echo_header ();
-
-/* Uncomment this code once we have access to the UL DB
 if (!$dbinfo->connect ())
 {
 	echo "Couldn't connect to database.";
 	echo_footer ();
 	return;
 }
-*/
+$dbinfo->update_time ();
 
-$table = new table_common_t ();
-$table->init ("tbl_std");
-echo $table->table_begin ();
-echo $table->table_head_begin ();
-echo $table->tr_begin ();
-echo $table->td_span ("Login", "", 2);
-echo $table->tr_end ();
-echo $table->table_body_begin ();
-echo form_begin ("index.php", "post");
-echo $table->tr ($table->td ("Username").
-		 $table->td (text_input ("user", "")));
-echo $table->tr ($table->td ("Password").
-		 $table->td (password_input ("pass", "")));
-echo $table->tr ($table->td_span (submit_input ("Login"), "", 2));
-echo form_end ();
-echo $table->table_body_end ();
-echo $table->table_end ();
+echo "Current Time? ".$dbinfo->day ()." ".$dbinfo->hour ()." ".$dbinfo->minute ()."<br/>";
+echo "Logged in? ".(($dbinfo->logged_in ())? "True" : "False")."<br/>";
+echo "User? ".$dbinfo->user ()."<br/>";
+echo "Admin? ".(($dbinfo->is_admin ())? "True" : "False")."<br/>";
+
+echo current_script ();
 
 echo_footer ();
+$dbinfo->close ();
 
 ?>
