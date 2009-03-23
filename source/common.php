86a0e583d7c546dea15c1f572841fd818af6a513
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-03-23 02:06:24 +0000

    Updated the echo_header() function to take an argument of a dbinfo_t so that customized navigation can be done. Did a few updates to the navigation based on if the current user is logged in or not. Added a function current_script() which will return the basename of the current executing script. Added a function redirect() that will redirect to a supplied url (such as after logging in). Added two functions get() and post() which when supplied a key will return the value from the respective variables  and

--- source/common.php
+++ source/common.php
@@ -4,8 +4,8 @@ include_once ("form_common.php");
 include_once ("table_common.php");
 include_once ("list_common.php");
 
-  // Call at the beginning of a script to output html header information including navbar
-function echo_header ()
+// Call at the beginning of a script to output html header information including navbar
+function echo_header ($dbinfo)
 {
 	echo <<<HEREDOC
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -21,8 +21,27 @@ function echo_header ()
 HEREDOC;
 	echo_div ("container");
 	echo_div ("navbar");
-	echo ul_wrap (li_wrap (href ("index.php", "HOME")).
-		      li_wrap (href ("login.php", "LOGIN")));
+	if (!$dbinfo->logged_in ())
+	{
+		$nav = "";
+		if (current_script () != "index.php")
+			$nav = $nav.li_wrap (href ("index.php", "Home"));
+		if (current_script () != "login.php")
+			$nav = $nav.li_wrap (href ("login.php", "Login"));
+		if (current_script () != "register.php")
+			$nav = $nav.li_wrap (href ("register.php", "Register"));
+
+		echo ul_wrap ($nav);
+	}
+	else
+	{
+		echo ul_wrap (li_wrap ($dbinfo->name ()." (".$dbinfo->user ().")"));
+		$nav = "";
+		if (current_script () != "index.php")
+			$nav = $nav.li_wrap (href ("index.php", "Home"));
+		$nav = $nav.li_wrap (href ("logout.php", "Logout"));
+		echo ul_wrap ($nav);
+	}
 	end_div ();
 	echo_div ("content");
 	echo "<!-- END HEADER -->\n";
@@ -41,6 +60,12 @@ function echo_footer ()
 HEREDOC;
 }
 
+// determine the current executing script
+function current_script ()
+{
+	return basename ($_SERVER["SCRIPT_NAME"]);
+}
+
 // Quick echo of a special div area
 function echo_div ($name)
 {
@@ -56,9 +81,30 @@ function end_div ()
 	echo "</div>\n";
 }
 
+// make a quick link
 function href ($url, $text)
 {
 	return "<a href=\"$url\">$text</a>\n";
+}
+
+// Make the user go to a different page
+function redirect ($url)
+{
+	header ("Location: $url");
+}
+
+// Get from the GET variable
+function get ($key)
+{
+	$val = $_GET[$key];
+	return $val;
+}
+
+// Get from the POST variable
+function post ($key)
+{
+	$val = $_POST[$key];
+	return $val;
 }
 
 ?>
