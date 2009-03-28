bcc8d918c329f97f8a12e01104910f2407f8dba9
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-03-28 16:43:25 +0000

    Updated logout to perform correctly with the new changes in common.php

--- source/logout.php
+++ source/logout.php
@@ -1,11 +1,9 @@
 <?php
 include_once ("common.php");
 
-// Need to handle saving the user activity of logout
-
 $dbinfo = new dbinfo_t ();
 $dbinfo->init ();
+if (!$dbinfo->connect ())
+	die ("Couldn't connect to database.");
 $dbinfo->logout ();
-
-redirect ("index.php");
 ?>
