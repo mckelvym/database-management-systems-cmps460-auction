a728c70a12414c5aa2f3424a7619ac39c507d010
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-03-28 16:52:05 +0000

    echo_header(): now takes $dbinfo and calls init() and connect() to connect to the database. Also will update the current time. Updated to show a status area and updated the navigation area to have links to various resources based on logged in, logged in and admin, or visitor. echo_footer(): now takes $dbinfo and calls close()
    Added cout(): which will take a string and append the html tag <br/> - which is just a shorter way of echoing things

--- source/common.php
+++ source/common.php
@@ -7,6 +7,11 @@ include_once ("list_common.php");
 // Call at the beginning of a script to output html header information including navbar
 function echo_header ($dbinfo)
 {
+	$dbinfo->init ();
+	if (!$dbinfo->connect ())
+		die ("Couldn't connect to database.");
+	$dbinfo->update_time ();
+
 	echo <<<HEREDOC
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -20,26 +25,85 @@ function echo_header ($dbinfo)
 
 HEREDOC;
 	echo_div ("container");
-	echo_div ("navbar");
-	if (!$dbinfo->logged_in ())
+
+	// HEADER AREA
+	echo_div ("header");
+	echo "iBay";
+	end_div ();
+
+	// STATUS AREA
+	echo_div ("status");
+	$day = $dbinfo->day () + 0;
+	$hour = $dbinfo->hour ();
+	$minute = $dbinfo->minute ();
+	if ($dbinfo->logged_in ())
 	{
-		$nav = "";
+		$realname = $dbinfo->realname ();
+		$username = $dbinfo->username ();
+		$logoutlink = href ("logout.php", "Logout");
+		cout ("Welcome, $realname. [$username] $logoutlink");
+	}
+	else
+	{
+		$loginlink = href ("login.php", "Login");
+		$reglink= href ("registration.php", "Register");
+		cout ("Welcome! $loginlink or $reglink");
+	}
+	echo ("Day: $day, Time: $hour:$minute");
+	if ($dbinfo->is_admin ())
+	{
+		echo "<br/>[Box to increase the time]";
+	}
+	end_div ();
+
+	// NAVIGATION AREA
+	echo_div ("navbar");
+	if ($dbinfo->logged_in ())
+	{
+		if ($dbinfo->is_admin ())
+			$nav = "Admin Panel<br/>";
+		else
+			$nav = "User Panel<br/>";
+
 		if (current_script () != "index.php")
 			$nav = $nav.li_wrap (href ("index.php", "Home"));
-		if (current_script () != "login.php")
-			$nav = $nav.li_wrap (href ("login.php", "Login"));
-		if (current_script () != "register.php")
-			$nav = $nav.li_wrap (href ("register.php", "Register"));
+		$nav = $nav.li_wrap (href ("registration.php?mode=view", "Account"));
+		$nav = $nav.li_wrap (href ("profile.php", "Profile"));
+		$nav = $nav.li_wrap (href ("profile.php?mode=browse", "All Profiles"));
+		if ($dbinfo->is_admin ())
+			$nav = $nav.li_wrap (href ("registration.php?mode=browse", "All Accounts"));
 
+		$nav = $nav.li_wrap (href ("category.php", "All Categories"));
+		$nav = $nav.li_wrap (href ("category.php?view=art", " - Art"));
+		$nav = $nav.li_wrap (href ("category.php?view=books", " - Books"));
+		$nav = $nav.li_wrap (href ("category.php?view=clothes", " - Clothes"));
+		$nav = $nav.li_wrap (href ("category.php?view=collectibles", " - Collectibles"));
+		$nav = $nav.li_wrap (href ("category.php?view=electronics", " - Electronics"));
+		$nav = $nav.li_wrap (href ("category.php?view=entertainment", " - Entertainment"));
+		$nav = $nav.li_wrap (href ("category.php?view=jewelry", " - Jewelry"));
+		$nav = $nav.li_wrap (href ("category.php?view=sporting_goods", " - Sporting Goods"));
+		$nav = $nav.li_wrap (href ("category.php?view=toys", " - Toys"));
+		$nav = $nav.li_wrap (href ("itemlisting.php?mode=new", "Create Auction"));
+
+		if ($dbinfo->is_admin ())
+		{
+			$nav = $nav.li_wrap ("Database:");
+			$nav = $nav.li_wrap (href ("viewtable.php?name=user", "User Table"));
+			$nav = $nav.li_wrap (href ("viewtable.php?name=user_activity", "Activity Table"));
+			$nav = $nav.li_wrap (href ("viewtable.php?name=item_listing", "Auction Table"));
+			$nav = $nav.li_wrap (href ("viewtable.php?name=bids_on", "Bid Table"));
+		}
 		echo ul_wrap ($nav);
 	}
 	else
 	{
-		echo ul_wrap (li_wrap ($dbinfo->name ()." (".$dbinfo->user ().")"));
-		$nav = "";
+		$nav = "Visitor<br/>";
 		if (current_script () != "index.php")
 			$nav = $nav.li_wrap (href ("index.php", "Home"));
-		$nav = $nav.li_wrap (href ("logout.php", "Logout"));
+		if (current_script () != "login.php")
+			$nav = $nav.li_wrap (href ("login.php", "Login"));
+		if (current_script () != "registration.php")
+			$nav = $nav.li_wrap (href ("registration.php", "Register"));
 		echo ul_wrap ($nav);
 	}
 	end_div ();
@@ -48,7 +112,7 @@ HEREDOC;
 }
 
 // Call at the end of a script for closing html footer information
-function echo_footer ()
+function echo_footer ($dbinfo)
 {
 	echo "<!-- FOOTER -->\n";
 	end_div ();
@@ -58,6 +122,7 @@ function echo_footer ()
 </html>
 
 HEREDOC;
+	$dbinfo->close ();
 }
 
 // determine the current executing script
@@ -105,6 +170,18 @@ function post ($key)
 {
 	$val = $_POST[$key];
 	return $val;
+}
+
+// Heading level 1
+function h1 ($text)
+{
+	return "<h1>$text</h1>\n";
+}
+
+// an echo with a <br/> at the end
+function cout ($str)
+{
+	echo "$str"."<br/>\n";
 }
 
 ?>
