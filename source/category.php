9a55fc4a201c549a1ccec8c2c8702d39fd4c9ad5
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-04-04 22:04:57 +0000

    Updates from our discussion - has working query where the data is displayed. Trey will continue the work on this script.

--- source/category.php
+++ source/category.php
@@ -15,8 +15,8 @@ if ($dbinfo->logged_in ())
 	{
 		print '<font size="5" color="blue">';
 		print "Art</font><br><br>";
-		
-    	echo "<a href=category.php?view=art&sortby=time_remaining>
+
+		echo "<a href=category.php?view=art&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=art&sortby=title>
 		        Sort by Title</a> |
@@ -24,15 +24,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=art&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Art";
-    }
-
+	}
 	else if ($view == "books")
 	{
 		print '<font size="5" color="blue">';
 		print "Books</font><br><br>";
-		
+
 		echo "<a href=category.php?view=books&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=books&sortby=title>
@@ -41,15 +39,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=books&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Books";
-    }
-    
+	}
 	else if ($view == "clothes")
 	{
 		print '<font size="5" color="blue">';
 		print "Clothes</font><br><br>";
-		
+
 		echo "<a href=category.php?view=clothes&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=clothes&sortby=title>
@@ -58,15 +54,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=clothes&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Clothes";
-    }
-    
+	}
 	else if ($view == "collectibles")
 	{
 		print '<font size="5" color="blue">';
 		print "Collectibles</font><br><br>";
-		
+
 		echo "<a href=category.php?view=collectibles&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=collectibles&sortby=title>
@@ -75,15 +69,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=collectibles&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Collectibles";
-    }
-    
+	}
 	else if ($view == "electronics")
 	{
 		print '<font size="5" color="blue">';
 		print "Electronics</font><br><br>";
-		
+
 		echo "<a href=category.php?view=electronics&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=electronics&sortby=title>
@@ -92,15 +84,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=electronics&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Electronics";
-    }
-    
+	}
 	else if ($view == "entertainment")
 	{
 		print '<font size="5" color="blue">';
 		print "Entertainment</font><br><br>";
-		
+
 		echo "<a href=category.php?view=entertainment&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=entertainment&sortby=title>
@@ -109,15 +99,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=entertainment&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-
 		$category = "Entertainment";
-    }
-    
+	}
 	else if ($view == "jewelry")
 	{
 		print '<font size="5" color="blue">';
 		print "Jewelry</font><br><br>";
-		
+
 		echo "<a href=category.php?view=jewelry&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=jewelry&sortby=title>
@@ -126,15 +114,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=jewelry&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Jewelry";
-    }
-    
+	}
 	else if ($view == "sporting_goods")
 	{
 		print '<font size="5" color="blue">';
 		print "Sporting Goods</font><br><br>";
-		
+
 		echo "<a href=category.php?view=sporting_goods&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=sporting_goods&sortby=title>
@@ -143,15 +129,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=sporting_goods&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Sporting Goods";
-    }
-    
+	}
 	else if ($view == "toys")
 	{
 		print '<font size="5" color="blue">';
 		print "Toys</font><br><br>";
-		
+
 		echo "<a href=category.php?view=toys&sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?view=toys&sortby=title>
@@ -160,15 +144,13 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?view=toys&sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-		        
 		$category = "Toys";
-    }
-    
+	}
 	else
 	{
 		print '<font size="5" color="blue">';
 		print "All Categories</font><br><br>";
-		
+
 		echo "<a href=category.php?sortby=time_remaining>
 		        Sort by Time Remaining</a> |
 		      <a href=category.php?sortby=title>
@@ -177,69 +159,77 @@ if ($dbinfo->logged_in ())
 		        Sort by Seller</a> |
 		      <a href=category.php?sortby=current_bid>
 		        Sort by Current Bid (Lowest Bid at Top)</a><br>";
-    }
-    
+	}
+
 	// Select the database
-		        
+
 	if ($sortby == "time_remaining")
 	{
-        $query = "select title, seller, end_day, end_hour, end_minute,
+		$query = "select title, seller, end_day, end_hour, end_minute,
                  current_price from item_listing where category =
                  '$category' AND end_day >= '$day' AND end_hour >=
                  '$hour' AND end_minute > '$minute' order by end_day
                  desc, end_hour desc, end_minute desc";
-    }
-    	
+	}
+
 	else if ($sortby == "title")
 	{
-        $query = "select title, seller, end_day, end_hour, end_minute,
+		$query = "select title, seller, end_day, end_hour, end_minute,
                  current_price from item_listing where category =
                  '$category' AND end_day >= '$day' AND end_hour >=
                  '$hour' AND end_minute > '$minute' order by title";
-    }
-    	
+	}
+
 	else if ($sortby == "seller")
 	{
-        $query = "select title, seller, end_day, end_hour, end_minute,
+		$query = "select title, seller, end_day, end_hour, end_minute,
                  current_price from item_listing where category =
                  '$category' AND end_day >= '$day' AND end_hour >=
                  '$hour' AND end_minute > '$minute' order by seller";
-    }
-    	
+	}
+
 	else if ($sortby == "current_bid")
 	{
-        $query = "select title, seller, end_day, end_hour, end_minute,
+		$query = "select title, seller, end_day, end_hour, end_minute,
                  current_price from item_listing where category =
                  '$category' AND end_day >= '$day' AND end_hour >=
                  '$hour' AND end_minute > '$minute' order by
                  current_bid";
-    }	
-		
+	}
+
 	else
 	{
-         $query = "select title, seller, end_day, end_hour, end_minute,
+		if (empty($view))
+			$query = "select title, seller, end_day, end_hour, end_minute,
+                  current_price from item_listing";
+		else
+			$query = "select title, seller, end_day, end_hour, end_minute,
                   current_price from item_listing where category =
-                  '$category' AND end_day >= '$day' AND end_hour >=
-                  '$hour' AND end_minute > '$minute'";
-     }
+                  '$category'";
 
+	}
 	// Run the query
 	$results_id = mysql_query($query);
 	if($results_id)
 	{
-		// Get each row of the result, assign a variable to each
-		// attribute in the row, and echo the data with labels
-		while (list($title, $seller, $end_day, $end_hour,$end_min,
-		            $curr_price) = mysql_fetch_row($results_id))
-		{
-			echo "<pre>";
-			echo "Title:           $title<br>";
-			echo "Seller:          $seller<br>";
-			echo "End Day:         $end_day<br>";
-			echo "End Hour:        $end_hour<br>";
-			echo "End Minute:      $end_min<br>";
-			echo "Current Price:   $curr_price<br>";
-			echo "</pre>";
+		if (mysql_num_rows($results_id) == 0){
+			echo "No data.";
+		}
+		else{
+			// Get each row of the result, assign a variable to each
+			// attribute in the row, and echo the data with labels
+			while (list($title, $seller, $end_day, $end_hour,$end_min,
+				    $curr_price) = mysql_fetch_row($results_id))
+			{
+				echo "<pre>";
+				echo "Title:           $title<br>";
+				echo "Seller:          $seller<br>";
+				echo "End Day:         $end_day<br>";
+				echo "End Hour:        $end_hour<br>";
+				echo "End Minute:      $end_min<br>";
+				echo "Current Price:   $curr_price<br>";
+				echo "</pre>";
+			}
 		}
 	}
 	else if ($dbinfo->debug ())
