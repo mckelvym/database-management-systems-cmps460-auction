a3ed029bb3a4bb389a4a7e977419339e4190b1b7
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-04-13 15:35:01 +0000

    Added initialization of some variables from GET in the top of the file.

--- source/itemlisting.php
+++ source/itemlisting.php
@@ -4,6 +4,12 @@ include_once ("common.php");
 $dbinfo = new dbinfo_t ();
 echo_header ($dbinfo);
 $mode = get ("mode");
+$title = get ("title");
+$seller = get ("seller");
+$category = get ("category");
+$end_day = get ("end_day");
+$end_hour = get ("end_hour");
+$end_min = get ("end_minute");
 
 if (!$dbinfo->logged_in ())
 	redirect ("index.php");
