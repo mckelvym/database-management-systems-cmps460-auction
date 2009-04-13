e86645514ad50bd69387fd65c7d08fcaf49a690a
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-04-13 20:14:52 +0000

    Added styles for item listing. Added script for viewing pictures in a category.

---
+++ source/category_pictures.php
@@ -0,0 +1,40 @@
+<?php
+include_once ("common.php");
+
+$dbinfo = new dbinfo_t ();
+echo_header ($dbinfo);
+$current_script = current_script ();
+$mode = get ("mode");
+
+if (empty ($mode))
+{
+	cout ("Pick a category:");
+
+	$options = "";
+	$options = $options.option ("Art", "Art");
+	$options = $options.option ("Books", "Books");
+	$options = $options.option ("Clothes", "Clothes");
+	$options = $options.option ("Collectibles", "Collectibles");
+	$options = $options.option ("Electronics", "Electronics");
+	$options = $options.option ("Entertainment", "Entertainment");
+	$options = $options.option ("Jewelry", "Jewelry");
+	$options = $options.option ("Sporting Goods", "Sporting Goods");
+	$options = $options.option ("Toys", "Toys");
+	echo form_begin ("$current_script?mode=view", "post");
+	echo select ("category", $options);
+	echo submit_input ("Select");
+}
+else
+{
+	$file = strtolower (post ("category"));
+	for ($i = 1; $i <= 3; $i++)
+	{
+		cout (local_img ("$file$i.jpg"));
+		cout ("$file$i.jpg");
+		cout ("");
+	}
+}
+
+echo_footer ($dbinfo);
+
+?>
--- source/viewtable.php
+++ source/viewtable.php
@@ -14,22 +14,22 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
 	{
 		print '<font size="5" color="blue">';
 		print "User Data</font><br><br></font>";
-		
-		
+
+
 		echo "<a href=viewtable.php?name=user&order=username&desc=0>
-		        Sort by Username Ascending</a> | 
+		        Sort by Username Ascending</a> |
 		      <a href=viewtable.php?name=user&order=username&desc=1>
 		        Sort by Username Descending</a><br>";
-		        
+
 		// Select the database
-		        
+
 		if ($order == "username")
 		{
             if ($desc == "0")
             {
                 $query="select * from $table order by username";
             }
-            
+
             else if ($desc == "1")
             {
                 $query="select * from $table order by username desc";
@@ -39,7 +39,7 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
                 $query="select * from $table order by username";
             }
 		}
-		
+
 		else $query="select * from $table";
 
 		// Run the query
@@ -62,44 +62,44 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
 		        echo $table->tr ($table->td_span ("Registered User of iBay", "", 6));
 	            echo $table->tr_end ();
 	            echo $table->table_head_end ();
-	            
-	            echo $table->table_body_begin ();				     
+
+	            echo $table->table_body_begin ();
 		        echo $table->tr ($table->td ("Username").
 		                         $table->td ($username));
 				echo $table->tr ($table->td ("Password").
-		                         $table->td ($passwd));		 
+		                         $table->td ($passwd));
 				echo $table->tr ($table->td ("Admin?").
-		                         $table->td ($is_admin));					 
+		                         $table->td ($is_admin));
 				echo $table->tr ($table->td ("Name").
-		                         $table->td ($name));					 
+		                         $table->td ($name));
 				echo $table->tr ($table->td ("D.O.B.").
-		                         $table->td ($dob));					 
+		                         $table->td ($dob));
 				echo $table->tr ($table->td ("Street").
-		                         $table->td ($street));					 
+		                         $table->td ($street));
 				echo $table->tr ($table->td ("City").
-		                         $table->td ($city));					 
+		                         $table->td ($city));
 				echo $table->tr ($table->td ("State").
-		                         $table->td ($state));					 
+		                         $table->td ($state));
 				echo $table->tr ($table->td ("Zip").
-		                         $table->td ($zip));					 
+		                         $table->td ($zip));
 				echo $table->tr ($table->td ("Phone").
-		                         $table->td ($phone));					 
+		                         $table->td ($phone));
 				echo $table->tr ($table->td ("Email").
-		                         $table->td ($email));					 
+		                         $table->td ($email));
 				echo $table->tr ($table->td ("Credit Card").
-		                         $table->td ($cc));					 
+		                         $table->td ($cc));
 				echo $table->tr ($table->td ("CC Number").
-		                         $table->td ($ccn));				 
+		                         $table->td ($ccn));
 				echo $table->tr ($table->td ("CC Exp.Date").
-		                         $table->td ($ccx));				 
+		                         $table->td ($ccx));
 				echo $table->tr ($table->td ("Picture").
-		                         $table->td ($picture));					 
+		                         $table->td ($picture));
 				echo $table->tr ($table->td ("Description").
-		                         $table->td ($descr));				
+		                         $table->td ($descr));
 				echo $table->table_body_end ();
-				
+
 	            echo $table->table_end ();
-	            echo "<br><br>";				
+	            echo "<br><br>";
 			}
 		}
 		else if ($dbinfo->debug ())
@@ -137,20 +137,20 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
 		        echo $table->tr ($table->td_span ("Activity Log Entry", "", 6));
 	            echo $table->tr_end ();
 	            echo $table->table_head_end ();
-	            
-	            echo $table->table_body_begin ();				     
+
+	            echo $table->table_body_begin ();
 		        echo $table->tr ($table->td ("Username").
 		                         $table->td ($username));
 				echo $table->tr ($table->td ("Day").
-		                         $table->td ($day));		 
+		                         $table->td ($day));
 				echo $table->tr ($table->td ("Hour").
-		                         $table->td ($hour));					 
+		                         $table->td ($hour));
 				echo $table->tr ($table->td ("Minute").
-		                         $table->td ($minute));					 
+		                         $table->td ($minute));
 				echo $table->tr ($table->td ("Activity").
-		                         $table->td ($activity));	
+		                         $table->td ($activity));
 				echo $table->table_body_end ();
-				
+
 	            echo $table->table_end ();
 	            echo "<br><br>";
 
@@ -169,21 +169,21 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
 	{
 		print '<font size="5" color="blue">';
 		print "Item Listing Data</font><br><br>";
-		
+
 		echo "<a href=viewtable.php?name=item_listing&order=seller&desc=0>
-		        Sort by Seller Ascending</a> | 
+		        Sort by Seller Ascending</a> |
 		      <a href=viewtable.php?name=item_listing&order=seller&desc=1>
 		        Sort by Seller Descending</a><br>";
-		        
+
 		// Select the database
-		        
+
 		if ($order == "seller")
 		{
             if ($desc == "0")
             {
                 $query="select * from $table order by seller";
             }
-            
+
             else if ($desc == "1")
             {
                 $query="select * from $table order by seller desc";
@@ -193,7 +193,7 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
                 $query="select * from $table order by seller";
             }
 		}
-		
+
 		else $query="select * from $table";
 
 		// Run the query
@@ -217,42 +217,42 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
 		        echo $table->tr ($table->td_span ("Auction Item Listing", "", 6));
 	            echo $table->tr_end ();
 	            echo $table->table_head_end ();
-	            
-	            echo $table->table_body_begin ();				     
+
+	            echo $table->table_body_begin ();
 		        echo $table->tr ($table->td ("Title").
 		                         $table->td ($title));
 				echo $table->tr ($table->td ("Seller").
-		                         $table->td ($seller));		 
+		                         $table->td ($seller));
 				echo $table->tr ($table->td ("Category").
-		                         $table->td ($category));					 
+		                         $table->td ($category));
 				echo $table->tr ($table->td ("End Day").
-		                         $table->td ($end_day));					 
+		                         $table->td ($end_day));
 				echo $table->tr ($table->td ("End Hour").
-		                         $table->td ($end_hour));					 
+		                         $table->td ($end_hour));
 				echo $table->tr ($table->td ("End Minute").
-		                         $table->td ($end_min));					 
+		                         $table->td ($end_min));
 				echo $table->tr ($table->td ("Description").
-		                         $table->td ($descr));					 
+		                         $table->td ($descr));
 				echo $table->tr ($table->td ("Shipping Cost").
-		                         $table->td ($ship_cost));					 
+		                         $table->td ($ship_cost));
 				echo $table->tr ($table->td ("Shipping Method").
-		                         $table->td ($ship_meth));					 
+		                         $table->td ($ship_meth));
 				echo $table->tr ($table->td ("Starting Price").
-		                         $table->td ($str_price));					 
+		                         $table->td ($str_price));
 				echo $table->tr ($table->td ("Current Price").
-		                         $table->td ($curr_price));					 
+		                         $table->td ($curr_price));
 				echo $table->tr ($table->td ("Picture").
-		                         $table->td ($pict));					 
+		                         $table->td ($pict));
 				echo $table->tr ($table->td ("Buyer").
-		                         $table->td ($buyer));				 
+		                         $table->td ($buyer));
 				echo $table->tr ($table->td ("Buyer Feedback Description").
-		                         $table->td ($buy_fdbk_descr));				 
+		                         $table->td ($buy_fdbk_descr));
 				echo $table->tr ($table->td ("Buyer Feedback Rating").
-		                         $table->td ($buy_fdbk_rate));					 
+		                         $table->td ($buy_fdbk_rate));
 				echo $table->tr ($table->td ("Seller Feedback Description").
-		                         $table->td ($sell_fdbk_descr));				
+		                         $table->td ($sell_fdbk_descr));
 				echo $table->table_body_end ();
-				
+
 	            echo $table->table_end ();
 	            echo "<br><br>";
 			}
@@ -270,21 +270,21 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
 	{
 		print '<font size="5" color="blue">';
 		print "Bid Data</font><br><br>";
-		
+
 		echo "<a href=viewtable.php?name=bids_on&order=username&desc=0>
-		        Sort by Username Ascending</a> | 
+		        Sort by Username Ascending</a> |
 		      <a href=viewtable.php?name=bids_on&order=username&desc=1>
 		        Sort by Username Descending</a><br>";
-		        
+
 		// Select the database
-		        
+
 		if ($order == "username")
 		{
             if ($desc == "0")
             {
                 $query="select * from $table order by username";
             }
-            
+
             else if ($desc == "1")
             {
                 $query="select * from $table order by username desc";
@@ -294,7 +294,7 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
                 $query="select * from $table order by username";
             }
 		}
-		
+
 		else $query="select * from $table";
 
 		// Run the query
@@ -317,34 +317,34 @@ if ($dbinfo->logged_in () && $dbinfo->is_admin ())
 		        echo $table->tr ($table->td_span ("Bid Information", "", 6));
 	            echo $table->tr_end ();
 	            echo $table->table_head_end ();
-	            
-	            echo $table->table_body_begin ();				     
+
+	            echo $table->table_body_begin ();
 		        echo $table->tr ($table->td ("Username").
 		                         $table->td ($username));
 		        echo $table->tr ($table->td ("Title").
 		                         $table->td ($title));
 				echo $table->tr ($table->td ("Seller").
-		                         $table->td ($seller));		 
+		                         $table->td ($seller));
 				echo $table->tr ($table->td ("Category").
-		                         $table->td ($category));					 
+		                         $table->td ($category));
 				echo $table->tr ($table->td ("End Day").
-		                         $table->td ($end_day));					 
+		                         $table->td ($end_day));
 				echo $table->tr ($table->td ("End Hour").
-		                         $table->td ($end_hour));					 
+		                         $table->td ($end_hour));
 				echo $table->tr ($table->td ("End Minute").
 		                         $table->td ($end_min));
 				echo $table->tr ($table->td ("Bid Day").
-		                         $table->td ($bid_day));					 
+		                         $table->td ($bid_day));
 				echo $table->tr ($table->td ("Bid Hour").
-		                         $table->td ($bid_hour));					 
+		                         $table->td ($bid_hour));
 				echo $table->tr ($table->td ("Bid Minute").
-		                         $table->td ($bid_min));					 
+		                         $table->td ($bid_min));
 				echo $table->tr ($table->td ("Bid Amount").
-		                         $table->td ($bid_amt));					 
+		                         $table->td ($bid_amt));
 				echo $table->tr ($table->td ("Display Notification").
-		                         $table->td ($disp_notif));				
+		                         $table->td ($disp_notif));
 				echo $table->table_body_end ();
-				
+
 	            echo $table->table_end ();
 	            echo "<br><br>";
 			}
