1978f65a7f692e0c45a2d0fd3f84adaa218f5f94
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2009-04-14 17:24:56 +0000

    Little bit of code cleanup.

--- source/profile.php
+++ source/profile.php
@@ -26,63 +26,54 @@ function verify_data ()
 }
 
 //If mode is empty, the user is current user
-if ($mode == "") {
+if ($mode == "")
+{
+	$user_name = $dbinfo->username ();
+}
 
-	$user_name = $dbinfo->username();
-
-}// Viewing someother's profile
-else if($mode == "view"||$mode == "edit")
+// Viewing someother's profile
+else if ($mode == "view" || $mode == "edit")
 {
 	$user_name = get("username");
 }
-if($mode == ""||$mode == "view" ){
 
+// Default view
+if ($mode == "" || $mode == "view" )
+{
 	echo_div ("scriptstatus");
-	if($user_name!=$home_user)
+	if($user_name != $home_user)
 		echo href ("$current_script?", "View My Profile");
 	else
 		echo href ("$current_script?mode=edit&username=$user_name", "Edit Profile");
 	end_div ();
-	echo h3 ("Profile");
-	?>
-		<table border="1" width="99%" cellpadding="10">
-			 <tr>
 
-			 <td width="5%" valign="top">
-			 <?php
-			 $table = new table_common_t();
+	$table = new table_common_t();
+	$table->init ("profile");
+
 	echo $table->table_begin();
+	echo $table->table_head_begin ();
+	if($user_name == $home_user)
+	{//You are visiting your own page
+		echo $table->tr ($table->td ("Your Profile"));
+	}
+	else
+	{
+		echo $table->tr ($table->td ($dbinfo->get_realname ($user_name)."'s Profile"));
+	}
+	echo $table->table_head_end ();
+	echo $table->table_body_begin ();
 	//Displaying the user picture
-	echo $table->tr_begin();
-	echo $table->td_span("<img src=\"images/" . $dbinfo->get_picture($user_name) . "\" WIDTH=100 HEIGHT=100> ");
-	echo $table->tr_end();
-	//Displaying the user picture
-	echo $table->tr_begin();
-	echo $table->td_span("<font size='2' face='Verdana''>" . $dbinfo->get_realname($user_name));
-	echo $table->tr_end();
+	echo $table->tr ($table->td (local_img ($dbinfo->get_picture($user_name))).
+			 $table->td ("About me: " . $dbinfo->get_userdesc($user_name)));
 	//Displaying the user description
-	echo $table->tr_begin();
-	echo $table->td_span("<font size='2' face='Verdana''>About me:" . $dbinfo->get_userdesc($user_name));
-	echo $table->tr_end();
+	echo $table->table_body_end ();
 	echo $table->table_end();
-	?>
-		</td>
-			  <td width="50%" valign="top">
-			  <?php
-			  if ($mode == "") {
 
-				  cout ("Welcome to your profile page.");
-
-			  }// Viewing someother's profile
-			  else if($mode == "view")
-			  {
-				  if($user_name == $home_user){//You are visinting your own page
-					  cout ("Welcome to your profile page.");
-				  }else
-					  cout ("Welcome to $user_name's profile page.");
-			  }
-
-
+	cout ("");
+	if($user_name != $home_user)
+		cout ("Feedback for ".$dbinfo->get_realname ($user_name).":");
+	else
+		cout ("Feedback for you:");
 
 #Query to check if the user is a seller and  has any feeback from buyer
 	$result_seller = $dbinfo->query ("select buyer,seller,buyerfeedbackforseller_rating,buyerfeedbackforseller_description from item_listing where seller ='$user_name' and buyerfeedbackforseller_rating!=-1 and buyerfeedbackforseller_description!='' and buyerfeedbackforseller_rating!=-1 order by end_day desc,end_hour desc,end_minute desc");
@@ -90,70 +81,77 @@ if($mode == ""||$mode == "view" ){
 	$result_buyer = $dbinfo->query("select buyer, seller, sellerfeedbackforbuyer_description from item_listing where buyer = '$user_name' and sellerfeedbackforbuyer_description!=''");
 	if(mysql_num_rows($result_seller) == 0 && mysql_num_rows($result_buyer) == 0)
 	{
-		echo div ("No feedback found!", "feedback");
+		echo div ("No feedback found.", "feedback");
 	}
-	if(mysql_num_rows($result_seller) != 0){
-		cout ("Feedback for items sold :");
-		while (list ($buyer, $seller, $buyerfeedbackforseller_rating, $buyerfeedbackforseller_description) = mysql_fetch_row($result_seller)) {
-
-
-			if ($seller == $user_name) {
-				$winner_realname = href ("profile.php?mode=view&username=$buyer", $dbinfo->get_realname ($buyer));
+	if(mysql_num_rows($result_seller) != 0)
+	{
+		cout ("Feedback for items sold:");
+		while (list ($buyer, $seller,
+			     $buyerfeedbackforseller_rating,
+			     $buyerfeedbackforseller_description)
+		       = mysql_fetch_row($result_seller))
+		{
+			if ($seller == $user_name)
+			{
+				$winner_realname = href ("profile.php?mode=view&username=$buyer",
+							 $dbinfo->get_realname ($buyer));
 				echo div ("$winner_realname has left the following feedback \"$buyerfeedbackforseller_description\"", "feedback");
-
 			}
-
 		}
 	}
-
-
-	if(mysql_num_rows($result_buyer)!=0)
+	if(mysql_num_rows($result_buyer) != 0)
 	{
-		cout ("");
-		cout ("Feedback for items bought :");
-		while (list ($buyer, $seller, $sellerfeedbackforbuyer_description) = mysql_fetch_row($result_buyer)) {
-
-
-			if ($buyer == $user_name) {
-				$seller_realname = href ("profile.php?mode=view&username=$seller", $dbinfo->get_realname ($seller));
+		cout ("Feedback for items bought:");
+		while (list ($buyer, $seller,
+			     $sellerfeedbackforbuyer_description)
+		       = mysql_fetch_row($result_buyer))
+		{
+			if ($buyer == $user_name)
+			{
+				$seller_realname = href ("profile.php?mode=view&username=$seller",
+							 $dbinfo->get_realname ($seller));
 				echo div ("$seller_realname has left the following feedback \"$sellerfeedbackforbuyer_description\"", "feedback");
-
 			}
-
 		}
 	}
+
+	// Bid History
 	cout ("");
+	if($user_name != $home_user)
+		cout ("Bid history for ".$dbinfo->get_realname ($user_name).":");
+	else
+		cout ("Bid history for you:");
 
-	$result_bid =  $dbinfo->query (" select username,item_seller, item_title,item_category ,item_end_day, item_end_hour, item_end_minute, bid_amount from bids_on where username = '$user_name' order by bid_amount desc");
-	if(mysql_num_rows($result_bid)==0)
+	$result_bid =  $dbinfo->query ("select username, item_seller, item_title, item_category, item_end_day, item_end_hour, item_end_minute, bid_amount
+from bids_on
+where 	username = '$user_name'
+order by bid_amount desc");
+	if(mysql_num_rows($result_bid) == 0)
 	{
-		echo div ("No bid history found!", "bidhistory");
-
+		echo div ("No bid history found.", "bidhistory");
 	}
-	else{
-		cout ("Bid history:");
-		while (list ($username, $item_seller, $item_title,$item_category, $item_end_day, $item_end_hour,$item_end_minute,$bid_amount) = mysql_fetch_row($result_bid)) {
-
-			$seller_realname = href ("profile.php?mode=view&username=$item_seller", $dbinfo->get_realname ($item_seller));
-
-			if($username == $home_user)
+	else
+	{
+		while (list ($username, $item_seller, $item_title, $item_category,
+			     $item_end_day, $item_end_hour, $item_end_minute,
+			     $bid_amount)
+		       = mysql_fetch_row($result_bid))
+		{
+			$seller_realname = href ("profile.php?mode=view&username=$item_seller",
+						 $dbinfo->get_realname ($item_seller));
+			if ($username == $home_user)
 				$bidder_realname = "You";
-			else{
-				$bidder_realname = href ("profile.php?mode=view&username=$username", $dbinfo->get_realname ($username));
-
+			else
+			{
+				$bidder_realname = href ("profile.php?mode=view&username=$username",
+							 $dbinfo->get_realname ($username));
 			}
 
 			echo div (span (format_time ($bid_day, $bid_hour, $bid_minute), "time").
 				  "$bidder_realname  made a bid of \$$bid_amount on $seller_realname's auction : \"".href ("itemlisting.php?mode=view&title=$item_title&seller=$item_seller&category=$item_category&end_day=$item_end_day&end_hour=$item_end_hour&end_minute=$item_end_minute", $item_title)."\".", "bidhistory");
-
 		}
 	}
-	?>
-		</td>
-			  </tr>
-			  </table>
-			  <?php
-			  }
+}
 else if ($mode == "browse")
 {
 	echo_div ("scriptstatus");
